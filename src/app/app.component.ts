593d7d0f748511ebb11913537411a791fe75c4a6
Sapfir0 <sapfr999999@yandex.ru>
2021-09-29 00:04:28 +0300

    feat: added web worker example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiInteractionService } from 'api_interaction_services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ApiInteractionService]
 })
-export class AppComponent {
-  title = 'my-sassy-app';
+export class AppComponent implements OnInit {
+  title = 'template-app';
+
+  // constructor(public api: ApiInteractionService) {
+  // }
+
+  ngOnInit() {}
+}
+
+if (typeof Worker !== 'undefined') {
+  // Create a new
+  const worker = new Worker(new URL('./app.worker', import.meta.url));
+  worker.onmessage = ({ data }) => {
+    console.log(`page got message: ${data}`);
+  };
+  worker.postMessage('hello');
+} else {
+  // Web Workers are not supported in this environment.
+  // You should add a fallback so that your program still executes correctly.
 }
