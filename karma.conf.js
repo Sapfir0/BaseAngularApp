9083902e06f75293220d57ab8d2d31fd0f635a91
Sapfir0 <sapfr999999@yandex.ru>
2021-09-28 18:54:14 +0300

    integrated scss

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angularTest'),
+      dir: require('path').join(__dirname, './coverage/my-sassy-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
