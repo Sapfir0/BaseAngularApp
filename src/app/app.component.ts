8a358c88d81792fd294fc1a2fc9d274870a75db9
alexander.yurev <alexander.yurev@nordclan.com>
2021-09-29 13:43:04 +0300

    feat: api_interaction_services integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-// import { ApiInteractionService } from 'api_interaction_services';
+import { ApiInteractionService } from 'api_interaction_services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  // providers: [ApiInteractionService]
+  providers: [
+    {
+      provide: ApiInteractionService,
+      useValue: new ApiInteractionService(
+        'https://raw.githubusercontent.com/AxolotlAPI/'
+      ),
+    },
+  ],
 })
 export class AppComponent implements OnInit {
   title = 'template-app';
   comments = ['message1', 'message2'];
-  // constructor(public api: ApiInteractionService) {
-  // }
+  constructor(public api: ApiInteractionService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(this.api);
+
+    console.log(this.api.get('/data/main/facts.txt'));
+  }
 }
 
 if (typeof Worker !== 'undefined') {
